550e21cd4bdd7a79f19a2adba5ad3e0b3bb8c8ab
saket mishra <sakkumishra1996@gmail.com>
2025-09-22 18:29:00 +0530

    Updated vercel.json for deployment

--- familysite/settings.py
+++ familysite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['family-website-lovct.vercel.app']
+ALLOWED_HOSTS = ['family-website-mkdv.vercel.app']
 
 # Application definition
 
